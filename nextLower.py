83d06f631b5f714b155b7e0cc4ea72026e6d912c
coderman1018 <96354459+coderman1018@users.noreply.github.com>
2022-01-15 12:44:26 -0800

    Create nextLower.py

---
+++ nextLower.py
@@ -0,0 +1,17 @@
+
+def next_lower(n):
+    sort = sorted(list(str(n)))
+    sort2 = "".join(sort)
+    lowest = int(sort2)
+    if lowest == n:
+        return -1
+    first = 0
+    second = 0
+    if sort2[0] == 0:
+        first = sort2[0]
+        second = sort2[1]
+        sort2[0] = second
+        sort2[1] = first
+    for x in reversed(range(lowest,n)):
+        if sorted(list(str(x))) == sort:
+            return x
